e9cf1c44ce902ef334e53fb0f3b04f360eb589b3
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2026-01-17 16:27:51 +0530

    feat: add Todo model with status, priority, and metadata enums and structs.

---
+++ apps/backend/internal/model/todo/todo.go
@@ -0,0 +1,50 @@
+package todo
+
+import (
+	"time"
+
+	"github.com/Sameer16536/ExecuTask/internal/model"
+	"github.com/google/uuid"
+)
+
+// Enum --> provides a way to define a set of named constants [type safety]
+type Status string
+
+const (
+	StatusDraft     Status = "draft"
+	StatusActive    Status = "active"
+	StatusCompleted Status = "completed"
+	StatusArchived  Status = "archived"
+)
+
+type Priority string
+
+const (
+	PriorityLow    Priority = "low"
+	PriorityMedium Priority = "medium"
+	PriorityHigh   Priority = "high"
+)
+
+// Nullable values will be of pointer type --> zero values will be nil
+type todo struct {
+	model.Base
+	UserID       string     `json:"userId" db:"user_id"`
+	Title        string     `json:"title" db:"title"`
+	Description  string     `json:"description" db:"description"`
+	Priority     Priority   `json:"priority" db:"priority"`
+	Status       Status     `json:"status" db:"status"`
+	DueDate      *time.Time `json:"dueDate" db:"due_date"`
+	CompletedAt  *time.Time `json:"completedAt" db:"completed_at"`
+	ParentTodoID *uuid.UUID `json:"parentTodoId" db:"parent_todo_id"`
+	CategoryID   *uuid.UUID `json:"categoryId" db:"category_id"`
+	Metadata     *Metadata  `json:"metadata" db:"metadata"`
+	SortOrder    int        `json:"sortOrder" db:"sort_order"`
+}
+
+// Embedded struct -->
+type Metadata struct {
+	Tags       []string `json:"tags"`
+	Reminder   *string  `json:"reminder"`
+	Color      *string  `json:"color"`
+	Difficulty *string  `json:"difficulty"`
+}
